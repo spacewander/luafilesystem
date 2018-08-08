498a895c8fde5818a33c275df65598493fb739ff
spacewander <spacewanderlzx@gmail.com>
2018-08-08 21:51:02 +0800

    test2

--- test_lock.lua
+++ test_lock.lua
@@ -1,10 +1,10 @@
-#!/usr/bin/env luajit
+#!/usr/bin/env lua
 
 local file = io.open('temp.txt', 'w')
 file:write('0123456789')
 file:close()
 
-io.popen('luajit lock_unlock.lua')
-os.execute('luajit lock_unlock.lua')
+io.popen('lua lock_unlock.lua')
+os.execute('lua lock_unlock.lua')
 
 os.remove('temp.txt')
