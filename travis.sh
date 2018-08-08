ade0102c674ecd11c888dc7f8a779c4522ea2078
spacewander <spacewanderlzx@gmail.com>
2018-08-08 21:43:39 +0800

    test

--- travis.sh
+++ travis.sh
@@ -4,7 +4,6 @@
 ./test_lock.lua > /tmp/lfs
 grep 'ERROR' /tmp/lfs && exit 1
 if [ "$(uname)" = "Linux" ]; then
-    sudo apt-get install valgrind
     LUA=luajit
     test -z "$(which $LUA)" && LUA=$PWD/lua_install/bin/lua
     valgrind --error-exitcode=42 --tool=memcheck \
